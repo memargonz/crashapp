4eca9bb485b311a279b604e4c3605ad3210812aa
Ian Baguio <ianrey_2004@yahoo.com>
2024-02-03 23:49:58 -0700

    chore: add routes

--- crash/src/app/app.routes.ts
+++ crash/src/app/app.routes.ts
@@ -1,3 +1,18 @@
 import { Routes } from '@angular/router';
+import { HomeContainerComponent } from './home-container/home-container.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: '/home',
+    pathMatch: 'full',
+  },
+  {
+    path: 'home',
+    component: HomeContainerComponent,
+  },
+  {
+    path: 'reporting',
+    component: HomeContainerComponent,
+  },
+];
